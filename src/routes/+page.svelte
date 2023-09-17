342c5236e30cdce3030765d747d63890de5617da
LuisKolb <kolb.luis@gmail.com>
2023-09-17 18:55:04 +0200

    move routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+    import ChatBot from "./ChatBot.svelte";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation!!</p>
+
+
+
+<ChatBot/>
--- src/routes/geo-convo/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-    import ChatBot from "./ChatBot.svelte";
-</script>
-
-<ChatBot/>
